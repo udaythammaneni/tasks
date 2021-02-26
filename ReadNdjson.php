d4e420f22006295f68b6fc23c3fddc2511fdff50
udaythammaneni <31239046+udaythammaneni@users.noreply.github.com>
2021-02-26 23:26:33 +0530

    Update ReadNdjson.php

--- ReadNdjson.php
+++ ReadNdjson.php
@@ -3,28 +3,30 @@
 //Generate A Dynamic Array and Convert it into JSON and Assign one New Array
 $encodeData = [];
 
-for($i=1;$i<=5;$i++){
+for ($i = 1;$i <= 5;$i++)
+{
 
-    $randomArray = ["id"=>$i,"name"=>"Text ".$i];
-    $encodeData[] = json_encode($randomArray,true); 
+    $randomArray = ["id" => $i, "name" => "Text " . $i];
+    $encodeData[] = json_encode($randomArray, true);
 
 }
 
 //Array convert inot String as New Line Formst
-$ndReady = implode("\n",$encodeData);
+$ndReady = implode("\n", $encodeData);
 /*{"id":1,"name":"Text 1"}
 {"id":2,"name":"Text 2"}
 {"id":3,"name":"Text 3"}
 {"id":4,"name":"Text 4"}
 {"id":5,"name":"Text 5"}*/
 //Data Save inot New File
-file_put_contents('listofusers.ndjson', $ndReady);//FILE_APPEND
+file_put_contents('listofusers.ndjson', $ndReady); //FILE_APPEND
 $readlines = file('listofusers.ndjson');
 
-foreach ($readlines as $line) {
-    
+foreach ($readlines as $line)
+{
+
     $data = json_decode($line, true);
-    echo "Name: ".$data['name']."<br/>";
+    echo "Name: " . $data['name'] . "<br/>";
 
 }
 
